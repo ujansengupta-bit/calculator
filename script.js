f1fc3e216eb12ee57886c00c75229e715a9af073
Ujan Sengupta <ujansenguptaofficial@gmail.com>
2025-12-04 21:05:50 +0530

    logic for maths operations

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const display = document.getElementById("display")
-display.innerHTML = "0";
-const clear = document.getElementById("clear")
-const equal = document.getElementById("equal")
+const display = document.getElementById("display");
+display.innerHTML ="";
 
 function click(tar){
     const id = document.getElementById(tar);
@@ -14,10 +12,102 @@ function click(tar){
 function erase(tar){
     const id = document.getElementById(tar);
     id.addEventListener("click", event=>{
-       display.innerHTML = "0";
+       display.innerHTML = "";
     })
 
 }
+
+function backspace(tar){
+    const id = document.getElementById(tar);
+    id.addEventListener("click", event =>{
+        let newstring = "";
+        let oldstring = display.innerHTML;
+
+        for(let i =0; i< oldstring.length -1; i++){
+            newstring += oldstring[i];
+        }
+        display.innerHTML = newstring;
+    })
+}
+
+
+function operate(a, operand, b){
+
+    if (operand === "/" && b === 0) {
+        display.innerHTML = "ERROR: Div by 0";
+        return; 
+    }
+    
+    if(operand === "+"){
+        display.innerHTML = add(a, b);
+    }
+    else if(operand === "-"){
+        display.innerHTML = substract(a, b);
+    } 
+    else if(operand === "*"){
+        display.innerHTML = multiply(a, b);
+    }
+    else{
+        display.innerHTML = divide(a, b);
+    }
+}
+
+function equals(tar){
+    const id = document.getElementById(tar);
+    id.addEventListener("click", event=>{
+        let string = display.innerHTML;
+        let operand = 0;
+        let index = -1;
+
+        for(let i=0; i< string.length; i++){
+            const char = string[i];
+
+            if(char ==='+' || char ==='*' || char ==='/'){
+                operand = char;
+                index = i;
+                break;
+            }
+
+            if(char ==='-' && index != 0){
+                operand = char;
+                index = i;
+                break;
+            }
+        }
+         if(index>0 && index<string.length){
+                let string_before="";
+                let string_after ="";
+
+                for(let j = 0; j< index; j++){
+                    string_before += string[j];
+                }
+                for(let j = index+1; j< string.length; j++){
+                    string_after += string[j];
+                }
+
+                let target1 = Number(string_before);
+                let target2 = Number(string_after);
+
+                operate(target1, operand, target2);
+        }
+
+    })
+}
+
+
+function add(target1, target2){
+        return Number(target1) + Number(target2);  
+}
+function substract(target1, target2){
+        return Number(target1) - Number(target2);  
+}
+function multiply(target1, target2){
+        return Number(target1) * Number(target2);  
+}
+function divide(target1, target2){
+        return Number(target1) / Number(target2);  
+}
+
 
 click("zero");
 click("one");
@@ -34,3 +124,5 @@ click("mul");
 click("plus");
 click("minus");
 erase("clear");
+backspace("del");
+equals("equal");
